1d3c9e01821da878ca538a9273c6de28478425d5
laviunee <24022612@vnu.edu.vn>
2025-10-20 09:37:45 +0700

    ✨ feat(core): add GameObject, and MovableObject classes

--- src/Core/GameObject.java
+++ src/Core/GameObject.java
@@ -1,4 +1,36 @@
 package Core;
 
-public class GameObject {
+import Utils.Vector2D;
+
+/**
+ * Lớp cha cho mọi đối tượng trong game
+ */
+public abstract class GameObject {
+    protected String name;
+    protected boolean active = true;
+    protected Vector2D position;
+
+    public GameObject(String name) {
+        this.name = name;
+        this.position = new Vector2D(0, 0);
+    }
+
+    public GameObject(String name, double x, double y) {
+        this(name);
+        this.position.set(x, y);
+    }
+
+    // Phương thức trừu tượng
+    public abstract void start();
+    public abstract void update(double deltaTime);
+
+    // Getter/Setter
+    public String getName() { return name; }
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+    public Vector2D getPosition() { return position; }
+
+    public void setPosition(double x, double y) {
+        position.set(x, y);
+    }
 }
--- src/Core/MovableObject.java
+++ src/Core/MovableObject.java
@@ -1,4 +1,44 @@
 package Core;
 
-public class MovableObject {
+import Utils.Vector2D;
+
+/**
+ * GameObject có khả năng di chuyển
+ */
+public abstract class MovableObject extends GameObject {
+    protected Vector2D velocity;
+    protected Vector2D acceleration;
+
+    public MovableObject(String name) {
+        super(name);
+        this.velocity = new Vector2D(0, 0);
+        this.acceleration = new Vector2D(0, 0);
+    }
+
+    public MovableObject(String name, double x, double y) {
+        super(name, x, y);
+        this.velocity = new Vector2D(0, 0);
+        this.acceleration = new Vector2D(0, 0);
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        // Cập nhật vận tốc theo gia tốc
+        velocity.x += acceleration.x * deltaTime;
+        velocity.y += acceleration.y * deltaTime;
+
+        // Cập nhật vị trí theo vận tốc
+        position.x += velocity.x * deltaTime;
+        position.y += velocity.y * deltaTime;
+    }
+
+    public Vector2D getVelocity() { return velocity; }
+    public void setVelocity(double vx, double vy) {
+        velocity.set(vx, vy);
+    }
+
+    public void applyForce(double fx, double fy) {
+        acceleration.x += fx;
+        acceleration.y += fy;
+    }
 }
