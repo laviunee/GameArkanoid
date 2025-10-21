0cd804c0ecaff3887fed9877e462a2ae9098ed7d
laviunee <24022612@vnu.edu.vn>
2025-10-21 10:35:58 +0700

    feat(scene): implement SceneManager for managing game scenes

--- src/Engine/SceneManager.java
+++ src/Engine/SceneManager.java
@@ -1,4 +1,15 @@
 package Engine;
 
-public class SceneManager {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Lớp trừu tượng cho tất cả các Scene trong game
+ */
+public abstract class SceneManager {
+    public abstract void start();
+    public abstract void update(double deltaTime);
+    public abstract void render();
+    public abstract void handleInput(KeyEvent event);
+    public abstract void cleanup();
 }
