2c1d1f9cfef32f954f2a603a2bfc874a370bcd36
laviunee <24022612@vnu.edu.vn>
2025-10-21 10:35:14 +0700

    feat(game): define GameState for controlling game flow

--- src/Engine/GameState.java
+++ src/Engine/GameState.java
@@ -1,4 +1,43 @@
 package Engine;
 
+/**
+ * Đại diện cho trạng thái hiện tại của game
+ */
 public class GameState {
+    public enum State {
+        MENU, PLAYING, PAUSED, GAME_OVER, LEVEL_COMPLETE
+    }
+
+    private State currentState = State.MENU;
+    private int currentLevel = 1;
+    private int score = 0;
+    private int lives = 3;
+    private boolean soundEnabled = true;
+
+    public State getCurrentState() { return currentState; }
+    public void setCurrentState(State state) { this.currentState = state; }
+
+    public int getCurrentLevel() { return currentLevel; }
+    public void setCurrentLevel(int level) { this.currentLevel = level; }
+
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+    public void addScore(int points) { this.score += points; }
+
+    public int getLives() { return lives; }
+    public void setLives(int lives) { this.lives = lives; }
+    public void loseLife() { this.lives--; }
+    public void addLife() { this.lives++; }
+
+    public boolean isSoundEnabled() { return soundEnabled; }
+    public void setSoundEnabled(boolean enabled) { this.soundEnabled = enabled; }
+
+    public boolean isGameOver() { return lives <= 0; }
+
+    public void reset() {
+        currentState = State.MENU;
+        currentLevel = 1;
+        score = 0;
+        lives = 3;
+    }
 }
