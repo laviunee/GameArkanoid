c3c938c2f23e4e01c61ddbf9b30b24de9299c06e
laviunee <24022612@vnu.edu.vn>
2025-11-07 01:29:34 +0700

    feat: 🎮 decrease power-up drop chance

--- src/Entities/Power/PowerFactory.java
+++ src/Entities/Power/PowerFactory.java
@@ -24,27 +24,22 @@ public class PowerFactory {
     }
 
     public static PowerUp createRandomPowerUp(double x, double y) {
-        int type = random.nextInt(5);
+        // 30% chance to spawn a power-up
+        if (random.nextDouble() > 0.3) {
+            return null; // không spawn
+        }
 
+        int type = random.nextInt(5); // chọn loại power-up
         switch (type) {
-            case 0:
-                return new ExpandPaddle(x, y);
-            case 1:
-                return new FastBall(x, y);
+            case 0: return new ExpandPaddle(x, y);
+            case 1: return new FastBall(x, y);
             case 2:
-                if (sharedBalls != null) {
-                    return new PowerUpMultiBall(x, y, sharedBalls);
-                } else {
-                    System.err.println("PowerFactory: balls list not set!");
-                    return null;
-                }
-            case 3:
-                return new PowerUpExtraLive(x, y, gameScene);
-            case 4:
-                return new PowerUpPierceBall(x, y);
-
-            default:
-                return null;
+                if (sharedBalls != null) return new PowerUpMultiBall(x, y, sharedBalls);
+                else { System.err.println("PowerFactory: balls list not set!"); return null; }
+            case 3: return new PowerUpExtraLive(x, y, gameScene);
+            case 4: return new PowerUpPierceBall(x, y);
+            default: return null;
         }
     }
+
 }
